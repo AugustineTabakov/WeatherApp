9516ae669e4430a2b02c2ed4e603a3bbbc9a2281
AugustineTabakov <163824029+AugustineTabakov@users.noreply.github.com>
2024-06-10 13:35:55 +0200

    Update DetailedViews.kt

--- app/src/main/java/com/tabakov/weatherapp/DetailedViews.kt
+++ app/src/main/java/com/tabakov/weatherapp/DetailedViews.kt
@@ -12,16 +12,13 @@ class DetailedViews : AppCompatActivity() {
         setContentView(R.layout.activity_detailed_views)
 
 
-//Declaration of button and textview to take the user back to the Main Menu. The averages to display the results for the weather.
 
         val btnMainMenu: Button = findViewById<Button>(R.id.btnMainMenu)
         val tvDetails = findViewById<TextView>(R.id.tvDetails)
         val displayData = intent.getStringExtra("DISPLAY_DATA")
         tvDetails.text = displayData
 
-        val tvAvg = findViewById<TextView>(R.id.tvAvg)
-        val displayAverage = intent.getStringExtra("DISPLAY_DATA_AVG")
-        tvAvg.text = displayAverage
+
 
 
         btnMainMenu.setOnClickListener {
@@ -30,4 +27,5 @@ class DetailedViews : AppCompatActivity() {
         }
     }
 }
+
 
